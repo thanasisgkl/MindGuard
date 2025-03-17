2be95e4257d6617314c78f0a96befba1b5c3f558
thanasisgkl <thanasisgkoulioumis@gmail.com>
2025-03-17 18:39:59 +0200

    Remove duplicate directory

--- mindguard-new/app/(auth)/login.tsx
+++
@@ -1,8 +0,0 @@
-import { View, StyleSheet, KeyboardAvoidingView, Platform, Alert } from 'react-native';
-import { Text, TextInput, Button, Divider } from 'react-native-paper';
-import { useState } from 'react';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { useGoogleAuth } from '../../../services/auth';
-import { signInWithGoogleSupabase } from '../../../services/supabase';
-import { useRouter } from 'expo-router';
-// ... existing code ... 
